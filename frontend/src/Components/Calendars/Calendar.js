78463778a9dd9f490a6b0dd4b9af15f0c016ced1
MessiasJunior <messiasjunior@alunos.utfpr.edu.br>
2019-07-25 14:52:56 -0300

    criação do calendario

---
+++ frontend/src/Components/Calendars/Calendar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Calendar } from "antd";
+
+export const Calendars = () => {
+  function onPanelChange(value, mode) {
+    console.log(value, mode);
+  }
+  return (
+    <>
+      <Calendar onPanelChange={onPanelChange} />
+    </>
+  );
+};
