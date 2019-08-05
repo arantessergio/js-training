c687c54b6eca88eaf9edb9466e43eb1956bbda53
unknown <jvitor.breda@gmail.com>
2019-08-05 13:33:14 -0300

    Mostrar tarefas no calendário

--- frontend/src/Components/Calendars/Calendar.js
+++ frontend/src/Components/Calendars/Calendar.js
@@ -1,12 +1,62 @@
-import React from "react";
-import "antd/dist/antd.css";
-import { Calendar } from "antd";
+import React, { useContext } from 'react';
+import 'antd/dist/antd.css';
+import { Calendar, Badge } from 'antd';
+import { TodoCtx } from '../../Hooks/TarefaContext'
 
 export const Calendars = () => {
-  function onPanelChange(value, mode) {
-    console.log(value, mode);
-  }
-  return (
-      <Calendar onPanelChange={onPanelChange} />
-  );
+	const ctx = useContext(TodoCtx)
+
+	const getListData = (value) => {
+		let listData;	
+		let ano, mes, dia
+		ctx.todoList.map(i => {
+			let e = new Date(i.time)
+			dia = e.getDate()
+			dia += 1
+			mes = e.getMonth()
+			ano = e.getFullYear()
+			if(value.date() === dia && value.month() === mes && value.year() === ano) {
+				listData = [
+					{ type: 'success', content: i.description },
+				]
+			}
+		})
+		return listData || [];
+	}
+
+	function dateCellRender(value) {
+		const listData = getListData(value);
+		return (
+			<ul className="events">
+				{listData.map(item => (
+					<div key={item.content}>
+						<Badge status={item.type} text={item.content} />
+					</div>
+				))}
+			</ul>
+		);
+	}
+
+	function getMonthData(value) {
+		if (value.month() === 8) {
+			return 1394;
+		}
+	}
+
+	function monthCellRender(value) {
+		const num = getMonthData(value);
+		return num ? (
+			<div className="notes-month">
+				<section>{num}</section>
+				<span>Backlog number</span>
+			</div>
+		) : null;
+	}
+
+	function onPanelChange(value, mode) {
+		console.log(value, mode);
+	}
+	return (
+		<Calendar onPanelChange={onPanelChange} dateCellRender={dateCellRender} monthCellRender={monthCellRender}/>
+	);
 };
